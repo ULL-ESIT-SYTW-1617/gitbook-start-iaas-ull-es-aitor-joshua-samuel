b0360d758ab33ccc8571a677d41fd8bda29f0d19
Samuel Ramos <alu0100815146@ull.edu.es>
2016-10-18 17:48:49 +0100

    añadiendo metodo deploy

--- module/iaas_deploy.js
+++ module/iaas_deploy.js
@@ -1,5 +1,18 @@
+var exec = require('child_process').exec;
+
 module "deploy" {
     export function deploy() {
-        
+      function puts(error, stdout, stderr) { console.log(stdout) }
+      exec("ssh sytw './scripts/pull_iass'", puts);
     }
 }
+/*
+var server = app.listen(80, function () {
+
+              var host = server.address().address
+              var port = server.address().port
+
+              console.log('Example app listening at http://%s:%s', host, port)
+
+            });
+*/
