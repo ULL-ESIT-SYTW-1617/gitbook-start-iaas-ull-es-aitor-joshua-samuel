ff0ed5ce9d9d41be4bf2d473aafee3b9da9fc5d6
Samuel Ramos <alu0100815146@ull.edu.es>
2016-10-20 10:31:03 +0100

    modificando el modulo y añadiendo tarea gulp

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -3,15 +3,20 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var shell = require('gulp-shell');
 var deploy = require('gulp-deploy-git');
+var iaas = require('iaas_deploy');
+var json = ('./package.json');
 
 gulp.task('deploy', function() {
   return gulp.src(['_book/*/*/*','_book/*/*','_book/*'])
     .pipe(ghPages());
 });
 
-
 gulp.task('deploy-github', shell.task([
   'git add .',
   'git commit -m "modificando"',
   'git push origin master'
 ]));
+
+gulp.task('deploy-iaas', function() {
+
+});
