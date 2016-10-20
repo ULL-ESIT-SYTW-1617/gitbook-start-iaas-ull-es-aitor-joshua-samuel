19a1791f9ea95081b5d1e3e7c5919f2f8a4126ba
Samuel Ramos <alu0100815146@ull.edu.es>
2016-10-20 10:00:44 +0100

    añadiendo modulo

--- module/iaas_deploy.js
+++ module/iaas_deploy.js
@@ -3,16 +3,6 @@ var exec = require('child_process').exec;
 module "deploy" {
     export function deploy() {
       function puts(error, stdout, stderr) { console.log(stdout) }
-      exec("ssh sytw './scripts/pull_iass'", puts);
+      exec("ssh sytw  'cd /home/usuario/prac3/practica-despliegues-en-iaas-y-heroku-aitor-joshua-samuel; git pull'", puts);
     }
 }
-/*
-var server = app.listen(80, function () {
-
-              var host = server.address().address
-              var port = server.address().port
-
-              console.log('Example app listening at http://%s:%s', host, port)
-
-            });
-*/
