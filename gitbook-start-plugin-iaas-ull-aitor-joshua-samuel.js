e065fcef1481a443bad5573684ac299ac5e190dd
Joshua Pérez García <alu0100818819@ull.edu.es>
2016-10-21 11:15:51 +0100

    Creando initialize

--- gitbook-start-plugin-iaas-ull-aitor-joshua-samuel.js
+++ gitbook-start-plugin-iaas-ull-aitor-joshua-samuel.js
@@ -1,12 +1,16 @@
 (() => {
     "use strict";
 
-    var exec = require('child_process').exec;
-
+    var exec = require('child_process').exec;    
+    var fs=require('fs');
     var iaas = () => {
 
         var initialize() => {
 
+            fs.appendFile('gulpfile.js', " var iaas = require('gitbook-start-plugin-iaas-ull-aitor-joshua-samuel')\nvar json = require('./package.json');\ngulp.task('deploy-iaas', function() {\n\tconsole.log(json.iaasip);\n\tconsole.log(json.iaaspath);\n\tiaas(json.iaasip,json.iaaspath);\n});", function(err) {
+            if (err)
+                console.error(err);
+            });
         }
 
         var deploy = (ipiaas, pathiaas) => {
