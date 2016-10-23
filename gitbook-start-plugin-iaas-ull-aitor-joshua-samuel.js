8caf7eefdf099910a330db54178dca0c09c1fd7a
Samuel Ramos <alu0100815146@ull.edu.es>
2016-10-23 10:34:29 +0100

    Corrigiendo errores de sintaxis

--- gitbook-start-plugin-iaas-ull-aitor-joshua-samuel.js
+++ gitbook-start-plugin-iaas-ull-aitor-joshua-samuel.js
@@ -1,26 +1,22 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
 
-    var exec = require('child_process').exec;
-    var fs=require('fs');
 
-    var iaas = () => {
 
-        var initialize() => {
+var initialize = () => {
 
-            fs.appendFile('gulpfile.js', " var iaas = require('gitbook-start-plugin-iaas-ull-aitor-joshua-samuel')\nvar json = require('./package.json');\ngulp.task('deploy-iaas', function() {\n\tconsole.log(json.iaasip);\n\tconsole.log(json.iaaspath);\n\tiaas(json.iaasip,json.iaaspath);\n});", function(err) {
-            if (err)
-                console.error(err);
-            });
-        }
+    fs.appendFile('gulpfile.js', " var iaas = require('gitbook-start-plugin-iaas-ull-aitor-joshua-samuel')\nvar json = require('./package.json');\ngulp.task('deploy-iaas', function() {\n\tconsole.log(json.iaasip);\n\tconsole.log(json.iaaspath);\n\tiaas.deploy(json.iaasip,json.iaaspath);\n});", function(err) {
+        if (err)
+            console.error(err);
+    });
+}
 
-        var deploy = (ipiaas, pathiaas) => {
-            function puts(error, stdout, stderr) {
-                console.log(stdout)
-            }
-            exec("ssh usuario@" + ipiaas + " 'cd " + pathiaas + "; git pull'", puts);
-        };
-        return {
-        initialize: initialize,
-        deploy: deploy
-      }
+var deploy = (ipiaas, pathiaas) => {
+    function puts(error, stdout, stderr) {
+        console.log(stdout)
     }
-    export.iaas = iaas;
+    exec("ssh usuario@" + ipiaas + " 'cd " + pathiaas + "; git pull'", puts);
+};
+
+exports.initialize = initialize;
+exports.deploy = deploy;
